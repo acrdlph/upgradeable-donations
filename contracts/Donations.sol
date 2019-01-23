69ca274efcbd4d641d1945dac968812ab2959f2c
acrdlph <achill@convergent.cx>
2019-01-23 21:46:51 +0100

    made contract initializable and added dummy upgrade

--- contracts/Donations.sol
+++ contracts/Donations.sol
@@ -3,11 +3,12 @@ pragma solidity 0.4.24;
 import "openzeppelin-eth/contracts/token/ERC721/ERC721Mintable.sol";
 import "openzeppelin-eth/contracts/math/SafeMath.sol";
 import "openzeppelin-eth/contracts/ownership/Ownable.sol";
+import "zos-lib/contracts/Initializable.sol";
 
 /**
  * @title Donations
  */
-contract Donations is Ownable {
+contract Donations is Initializable, Ownable {
 
     using SafeMath for uint256;
 
@@ -17,8 +18,17 @@ contract Donations is Ownable {
     // ERC721 non-fungible tokens to be emitted on donations.
     ERC721Mintable public token;
     uint256 public numEmittedTokens;
+    // These are our initial state variables:
+    uint256 public minDonation;
+    string public contractName;
+    // The initialize function acts as a constructor, that can be called from outside
+    function initialize(uint256 _minDonation, string _contractName) initializer public {
+        minDonation = _minDonation;
+        contractName = _contractName;
+    }
 
     function donate() public payable {
+        require(msg.value > minDonation, "Send more ETH!!");
         emit NewDonation(msg.sender, msg.value);
         emitUniqueToken(msg.sender);
     }
@@ -40,6 +50,10 @@ contract Donations is Ownable {
         uint256 value = address(this).balance;
         wallet.transfer(value);
         emit Withdrawal(wallet, value);
+    }
+
+    function reduceMinDonation() public {
+        minDonation -= 1;
     }
 }
 
